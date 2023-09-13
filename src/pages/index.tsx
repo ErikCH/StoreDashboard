b2ac7e346a4d20c72c48b470ee63128a36ec8b4d
Erik Hanchett <ehhanche@amazon.com>
2023-09-13 14:07:48 -0700

    Cleaned code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,56 +15,16 @@ import ProductsTable from "@/components/ProductsTable";
 
 export default function Home() {
   const [products, setProducts] = useState<Product[]>();
-  const [t, setT] = useState();
 
   useEffect(() => {
     async function grabProducts() {
       const allProducts = await API.graphql<GraphQLQuery<ListProductsQuery>>({
         query: queries.listProducts,
       });
-      // const data = Promise.all(
-      //   allProducts.data!.listProducts!.items.map(async (obj) => {
-      //     if (!obj?.genreID) return obj;
-      //     const l = {
-      //       ...obj,
-      //       genreID: await grabGenre(obj?.genreID),
-      //       platformID: await grabPlatform(obj?.platformID),
-      //     };
-      //     console.log("obj, obj", l);
-      //     return l;
-      //   })
-      // );
-      // console.log("h", await data);
-      // setProducts(allProducts.data?.listProducts?.items as Product[]);
       setProducts(allProducts.data?.listProducts?.items as Product[]);
-      // setT((await data).values as any);
-      // setProducts(data as unknown as Product[]);
     }
     grabProducts();
   }, []);
-  async function grabGenre(id: string) {
-    const params: GetGenreQueryVariables = {
-      id,
-    };
-    const genre = await API.graphql<GraphQLQuery<GetGenreQuery>>({
-      query: queries.getGenre,
-      variables: params,
-    });
-    console.log(genre);
-    return genre.data?.getGenre?.name;
-  }
-
-  async function grabPlatform(id: string) {
-    const params: GetPlatformQueryVariables = {
-      id,
-    };
-    const platform = await API.graphql<GraphQLQuery<GetPlatformQuery>>({
-      query: queries.getPlatform,
-      variables: params,
-    });
-    console.log(platform);
-    return platform.data?.getPlatform?.name;
-  }
 
   const { signOut } = useAuthenticator((context) => [context.signOut]);
   return (
