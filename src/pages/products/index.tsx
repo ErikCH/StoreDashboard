6361c52e9e17c158d205681e3f9f3aa14141aa9e
Erik Hanchett <icystorm@gmail.com>
2023-09-13 14:06:16 -0700

    updated text

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -5,10 +5,17 @@ import { useRouter } from "next/router";
 export default function Platform() {
   const router = useRouter();
   return (
-    <Flex alignItems={"center"} justifyContent="center" direction={"column"}>
+    <Flex
+      alignItems={"center"}
+      justifyContent="center"
+      direction={"column"}
+      width="650px"
+      margin="0 auto"
+    >
       <Heading level={1}>Create New Product</Heading>
       <ProductCreateForm
-        width="800px"
+        width={"100%"}
+        padding="2rem"
         border="1px solid black"
         borderRadius={"1 rem"}
         onSuccess={() => router.push("/")}
